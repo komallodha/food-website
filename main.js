bdecdd6e92578ae347f550ba08cdb6e09f9c4ea6
komal <komallodha9@gmail.com>
2022-07-15 20:21:03 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+let preveiwContainer = document.querySelector('.product-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.product-container .product').forEach(product => {
+    product.onclick = () => {
+        preveiwContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+        close.classList.remove('active');
+        preveiwContainer.style.display = 'none';
+    };
+});
+
+
+
+
+
+
+
+
+
